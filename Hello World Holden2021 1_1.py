f10624a56aec5f72c3c8014ccec47e8f9ec28f78
Holden Anderson <holden.anderson@tooeleschools.org>
2021-09-03 10:01:31 -0600

    qoutes. py created today 9/3/2021 by Holden Anderson, 2 other files: about me doc and HelloWOrld2021 py

---
+++ Hello World Holden2021 1_1.py
@@ -0,0 +1,39 @@
+#Holden Anderson
+
+#9-21
+
+#Hello World
+
+
+
+#imports section
+###import this
+##################
+
+#Main
+#Intro
+print("Personal Info \n for \n")
+
+#First Name
+#collect name
+firstName = input("Enter your First Name now")
+#Last Name
+#collect name
+lastName = input("Enter your LaSt Name now")
+#DOB
+#collect date of birth
+collect date of birth
+DOB = input("what is your birthday")
+#SSN
+
+print("https://youtu.be/dQw4w9WgXcQ")
+#Phone Number
+print("+435 778-330-2389")
+#Mother's Maiden Name
+print("Astley")
+#Debit Card number
+print("2389456789")
+#CVV number
+print("778")
+# hold open
+input()
---
+++ qoutes.py
@@ -0,0 +1,15 @@
+#Eric Broadbent
+#9-21
+
+#Random""
+
+import random
+
+#Main
+
+
+qoute1="I\'d look at myself in the mirror and be like:\n maybe I do look like I have a frog in my \n mouth\" \n \t \t \t -Tom Holland"
+print (qoute1)
+
+#end
+input("Press enter to exit")
