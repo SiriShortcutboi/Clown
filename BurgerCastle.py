787755c27d1ebf3baff2c1f9bfc68b56ba370433
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2021-10-29 00:40:23 -0600

    BurgerCastle working copy commit
    Holden Anderson, 10-21, home

--- BurgerCastle.py
+++ BurgerCastle.py
@@ -1,1 +1,15 @@
-print("Welcome to BurgerCastle")
+#Holden Anderson
+#10-21
+#BurgerCastle Assignment
+
+validOrders = ("burger","fries","salad","soda","milkshake")
+itemDescriptions = ("Half-pound burger","Large fries","Side salad", "Diet root beer", "Chocolate shake") 
+order = []
+
+print("Welcome to Burger Castle"
+print("Menu:" + validOrders)
+print("Please enter each item in your order. Press 'Enter' or type 'quit' on an empty line when done.")
+
+while (orderDone == "quit") or (orderDone != "burger" or "fries" or "salad" or "soda" or "milkshake")
+    
+    return order
