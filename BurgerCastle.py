54dd16ff23b9bea3d7bfd2b481aad84fb2c271db
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2021-10-29 02:21:12 -0600

    Burger Castle assignment update
    Holden Anderson, 10-21, home

--- BurgerCastle.py
+++ BurgerCastle.py
@@ -6,10 +6,34 @@ validOrders = ("burger","fries","salad","soda","milkshake")
 itemDescriptions = ("Half-pound burger","Large fries","Side salad", "Diet root beer", "Chocolate shake") 
 order = []
 
-print("Welcome to Burger Castle"
+#welcome
+print("Welcome to Burger Castle")
+#print menu items
 print("Menu:" + validOrders)
+#tell them to enter specific foods you have
 print("Please enter each item in your order. Press 'Enter' or type 'quit' on an empty line when done.")
 
-while (orderDone == "quit") or (orderDone != "burger" or "fries" or "salad" or "soda" or "milkshake")
+#until the order is done
+while orderDone != True:
+    # get the individual food items ordered 
+    orderValue = input("Enter item:")
+    #if the input is a carriage return or the phrase "quit", the order is set to done
+    if orderValue == "" or orderValue =="quit":
+        orderDone = True
+    else:
+        #if the individual food order matches a valid order, append the food to the order,
+        if orderValue in validOrders:
+            order.append(orderValue)
+        else:
+            #tell them we dont have the thing they said
+            print("Sorry, we don't sell" + orderValue)
+         # return order so we can do something with it
+        return order
+
+for item in order:
+    index = index(validOrders)
+    description = itemDescriptions[index]
+    print(description)
     
-    return order
+print("Thanks for visiting Burger Castle!")
+
